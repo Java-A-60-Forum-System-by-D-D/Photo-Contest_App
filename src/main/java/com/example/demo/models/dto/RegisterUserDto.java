ffeaa9b8086f35794931ff0c8162c09c0bf1a2d0
DesZafirova <deszafirova@icloud.com>
2024-08-26 15:27:33 +0300

    added user data transfer objects for registration

--- src/main/java/com/example/demo/models/dto/RegisterUserDto.java
+++ src/main/java/com/example/demo/models/dto/RegisterUserDto.java
@@ -1,9 +1,14 @@
 package com.example.demo.models.dto;
 
+import com.example.demo.models.validations.FieldMatch;
+import com.example.demo.models.validations.UniqueUserEmail;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
+@FieldMatch(first = "password", second = "confirmPassword", message = "The passwords do not match")
 public class RegisterUserDto {
     @NotNull
     private String username;
@@ -11,7 +16,9 @@ public class RegisterUserDto {
     private String password;
     @NotNull
     private String confirmPassword;
-    @NotNull
+    @NotEmpty(message = "User email should be provided.")
+    @Email(message = "User email should be valid.")
+    @UniqueUserEmail(message = "User email should be unique.")
     private String email;
     @NotNull
     private String firstName;
---
+++ src/main/java/com/example/demo/models/dto/UserViewDto.java
@@ -0,0 +1,11 @@
+package com.example.demo.models.dto;
+
+import lombok.Data;
+
+@Data
+public class UserViewDto {
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+}
