8f214a9f3f57d61989284e70fade57bcf8f71e0a
DesZafirova <deszafirova@icloud.com>
2024-08-27 11:39:47 +0300

    implementation of Scheduler for contest phases

--- src/main/java/com/example/demo/models/mappers/ContestMapper.java
+++ src/main/java/com/example/demo/models/mappers/ContestMapper.java
@@ -3,10 +3,13 @@ package com.example.demo.models.mappers;
 import com.example.demo.models.Contest;
 import com.example.demo.models.User;
 import com.example.demo.models.dto.ContestDto;
+import com.example.demo.models.dto.ContestViewDto;
 import com.example.demo.services.CategoryService;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 @Component
 public class ContestMapper {
     private final CategoryService categoryService;
@@ -22,7 +25,23 @@ public class ContestMapper {
         contest.setCategory(categoryService.getCategoryByName(contestDto.getCategory()));
         contest.setCreatedAt(LocalDateTime.now());
         contest.setOrganizer(user);
-
+        LocalDateTime startPhase1 = LocalDateTime.now().with(LocalTime.of(22, 0));
+        contest.setStartPhase1(startPhase1);
+        LocalDateTime startPhase2 = startPhase1.plusDays(2);
+        contest.setStartPhase2(startPhase2);
+        LocalDateTime startPhase3 = startPhase2.plusHours(12);
+        contest.setEndPhase2(startPhase3);
         return contest;
+    }
+    public ContestViewDto createContestViewDto(Contest contest) {
+        ContestViewDto contestViewDto = new ContestViewDto();
+        contestViewDto.setTitle(contest.getTitle());
+        contestViewDto.setDescription(contest.getDescription());
+        contestViewDto.setCategory(contest.getCategory().getName());
+        contestViewDto.setCreatorName(contest.getOrganizer().getFirstName() + " " + contest.getOrganizer().getLastName());
+        contestViewDto.setStartPhase1(contest.getStartPhase1());
+        contestViewDto.setStartPhase2(contest.getStartPhase2());
+        contestViewDto.setEndPhase2(contest.getEndPhase2());
+        return contestViewDto;
     }
 }
