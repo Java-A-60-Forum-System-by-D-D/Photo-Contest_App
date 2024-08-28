fc28564b6fafff859dc4331a34f15848a337657f
DesZafirova <deszafirova@icloud.com>
2024-08-28 12:36:11 +0300

    fixed entity duplicate exception for PhotoReview. handle bug for reviews - should be only in phase 2

--- src/main/java/com/example/demo/repositories/PhotoReviewRepository.java
+++ src/main/java/com/example/demo/repositories/PhotoReviewRepository.java
@@ -3,6 +3,14 @@ package com.example.demo.repositories;
 import com.example.demo.models.Category;
 import com.example.demo.models.PhotoReview;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+@Repository
 public interface PhotoReviewRepository  extends JpaRepository<PhotoReview,Long> {
+
+    @Query("SELECT r FROM PhotoReview r JOIN PhotoSubmission ps ON r.id = ps.id WHERE r.jury.id = :userId AND ps.id = :photoSubmissionId")
+    Optional<PhotoReview> findPhotoReviewByJuryHasAlreadyReviewedPhotoSubmission(@Param("userId") long userId,@Param("photoSubmissionId") long photoSubmissionId);
 }
--- src/main/java/com/example/demo/services/PhotoReviewService.java
+++ src/main/java/com/example/demo/services/PhotoReviewService.java
@@ -2,7 +2,10 @@ package com.example.demo.services;
 
 import com.example.demo.models.PhotoReview;
 import com.example.demo.models.PhotoSubmission;
+import com.example.demo.models.User;
 
 public interface PhotoReviewService {
     PhotoReview createPhotoReview(PhotoReview photoReview, PhotoSubmission photoSubmission);
+
+    PhotoReview handleUserReview(PhotoReview photoReview, PhotoSubmission photoSubmission, User user);
 }
