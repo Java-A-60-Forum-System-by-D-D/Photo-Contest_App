4fed3ff0f7e1e734eb0f05e45edf32d5c46272a2
DesZafirova <deszafirova@icloud.com>
2024-09-13 10:31:41 +0300

    fixed query

--- src/main/java/com/example/demo/repositories/NotificationRepository.java
+++ src/main/java/com/example/demo/repositories/NotificationRepository.java
@@ -4,12 +4,14 @@ import com.example.demo.models.Category;
 import com.example.demo.models.Notification;
 import com.example.demo.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    @Query("Select n from Notification n where n.recipient = ?1 and n.isRead = false")
     List<Notification> findAllByRecipientAndReadIsFalse(User recipient);
     List<Notification> findAllByRecipient(User recipient);
 }
